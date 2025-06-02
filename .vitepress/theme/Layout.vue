fd2643565ca5715fd6f968a9a03934a350ba36d5
lhuans <lhs.c@qq.com>
2025-06-02 16:58:04 -0700

    feat: 添加评论功能

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
-import { useData } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import Comment from './Comment.vue'
+import { useData, inBrowser } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import Giscus from '@giscus/vue';
+import { watch } from 'vue';
+
+const { page, isDark } = useData();
+const { Layout } = DefaultTheme;
+watch(isDark, (dark) => {
+  if (!inBrowser) return;
+
+  const iframe = document
+    .querySelector('giscus-widget')
+    ?.shadowRoot?.querySelector('iframe');
+
+  iframe?.contentWindow?.postMessage(
+    { giscus: { setConfig: { theme: dark ? 'dark' : 'light' } } },
+    'https://giscus.app'
+  );
+});
 </script>
 
 <template>
-    <DefaultTheme.Layout>
-        <template #doc-after>
-            <Comment />
-        </template>
-    </DefaultTheme.Layout>
+  <DefaultTheme.Layout>
+    <template #doc-after>
+      <Giscus
+        :key="page.filePath"
+        repo="lhuans/docs"
+        repo-id="R_kgDOOqP84g"
+        category="General"
+        category-id="DIC_kwDOOqP84s4Cq7iZ"
+        mapping="pathname"
+        strict="0"
+        reactions-enabled="1"
+        emit-metadata="0"
+        input-position="bottom"
+        theme="preferred_color_scheme"
+        lang="zh-CN"
+        loading="lazy"
+        crossorigin="anonymous"
+      />
+    </template>
+  </DefaultTheme.Layout>
 </template>
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,12 +1,11 @@
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import Layout from './Layout.vue'
-
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import Layout from './Layout.vue';
 
 export default {
   Layout,
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // 注册自定义全局组件
-  }
-} satisfies Theme
+  },
+} satisfies Theme;
